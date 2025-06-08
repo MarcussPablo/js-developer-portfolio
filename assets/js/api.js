68e31095fb130f5337335d1451fbefe9c8154999
Marcus <pavlolins@gmail.com>
2025-06-08 19:01:54 -0300

    teste-dois

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/MarcussPablo/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/MarcussPablo/js-developer-portfolio/refs/heads/master/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
